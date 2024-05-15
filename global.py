a81fb91d2107578fe2a94cb95239b6051475070f
Del Hatch <engineering_type@yahoo.com>
2024-05-15 11:46:39 -0700

    Removed unneeded shutdown line.

--- global.py
+++ global.py
@@ -5,4 +5,4 @@ p=subprocess.run(["python","/home/drh/PiPod_Zero2W/Sofware/main.py"])
 #if p.returncode == 1:
   #print("FAILED. Re-launching...")
   #p=subprocess.run(["python","/home/drh/PiPod_Zero2W/Sofware/main.py"])
-os.system("sudo shutdown now")
+#os.system("sudo shutdown now")
