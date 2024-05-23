35e8d72f7a58afe7219052d5d1ccf8982688094e
Del Hatch <engineering_type@yahoo.com>
2024-05-23 14:34:48 -0700

    Changed to user pi

--- Software/global.py
+++ Software/global.py
@@ -1,8 +1,8 @@
 import subprocess
 import os
 
-p=subprocess.run(["python","/home/drh/PiPod_Zero2W/Sofware/main.py"])
+p=subprocess.run(["python","/home/pi/PiPod_ePaper/Software/main.py"])
 #if p.returncode == 1:
   #print("FAILED. Re-launching...")
-  #p=subprocess.run(["python","/home/drh/PiPod_Zero2W/Sofware/main.py"])
+  #p=subprocess.run(["python","/home/pi/PiPod_ePaper/Software/main.py"])
 #os.system("sudo shutdown now")
